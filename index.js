409b5414def5c46a35b436670a6a2f812037a7da
ashutosh-alt <93521993+fablo-system@users.noreply.github.com>
2021-12-18 11:19:49 +0530

    IP

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = require('./app');
 require('dotenv').config()
 process.env.TZ = 'Asia/Calcutta';
 const port = process.env.PORT || 4001;
+const IP = process.env.IP || '127.0.0.1';
 const server = http.createServer(app);
 server.listen(port,() => {
-    console.log(`Fablo-User listening at http://localhost:${port}`)
+    console.log(`Fablo listening at http://${IP}:${port}`)
 });
